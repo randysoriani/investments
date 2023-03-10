9e5fc690520e1bca22b68e25e216b44dc2a19294
Randy Soriani <randysoriani@outlook.com>
2023-03-10 16:07:16 -0300

    add dotenv lib

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
+import { config } from 'dotenv'
 import express, { Request, Response } from 'express';
 
+config();
 const server = express();
 
 server.get('/', (req: Request, res: Response)=> {
@@ -7,4 +9,7 @@ server.get('/', (req: Request, res: Response)=> {
         
 })
 
-server.listen(3333);
+server.listen(
+    process.env.PORT,
+    () => { console.log('Server running at: localhost:' + process.env.PORT)}
+);
