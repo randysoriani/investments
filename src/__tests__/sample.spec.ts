c89d9c8ab1c8c4daa53cebaca77c7cae21da9b58
Randy Soriani <randysoriani@outlook.com>
2023-03-11 08:52:11 -0300

    remove sample.spec file

--- src/__tests__/sample.spec.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest'
-
-describe('Sample', () => {
-    it('Should return OK string', ()=> {
-        const string = 'OK'
-        expect(string).toBe('OK');
-    })
-})
